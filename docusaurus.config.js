7c8405872c511691779be22f601f8ce486132660
XIAO YANG <swagshaw1998@gmail.com>
2023-01-30 00:38:46 +0800

    Add visitor counters.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -57,92 +57,137 @@ const config = {
 
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      navbar: {
-        title: 'Awesome AI Engineering',
-        logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
-        },
-        items: [
-          {to: '/docs/course', label: 'Course', position: 'left'},
-          {to: '/docs/blog', label: 'Blog', position: 'left'},
-          {to: '/docs/talk', label: 'Talk', position: 'left'},
-          {to: '/docs/group', label: 'Group', position: 'left'},
-          {to: '/docs/paper/intro', label: 'Paper', position: 'left'},
-          {to: '/docs/tutorial/MLOps_101', label: 'Tutorial', position: 'left'},
-          // {
-          //   type: 'doc',
-          //   docId: 'paper/intro',
-          //   position: 'left',
-          //   label: 'Paper',
-          // },
-          // {
-          //   type: 'doc',
-          //   docId: 'tutorial/MLOps_101',
-          //   position: 'left',
-          //   label: 'Tutorial',
-          // },
-          // {to: '/blog', label: 'Blog', position: 'left'},
-          // {to: '/docs/paper', label: 'Paper', position: 'left'},
-          {
-            href: 'https://breezeml.ai',
-            label: 'BreezeML',
-            position: 'right',
-          },
-          {
-            href: 'https://github.com/HuaizhengZhang/Awesome-System-for-Machine-Learning',
-            label: 'GitHub',
-            position: 'right',
-          },
-        ],
-      },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Quick Start',
-                to: '/docs/quick_start',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'BreezeML',
-                href: 'https://breezeml.ai',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              // {
-              //   label: 'Blog',
-              //   to: '/blog',
-              // },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/HuaizhengZhang/Awesome-System-for-Machine-Learning',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} MLSysOps, Inc. Built with Docusaurus.`,
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-      },
-    }),
+        ({
+            navbar: {
+                title: 'Awesome AI Engineering',
+                logo: {
+                    alt: 'My Site Logo',
+                    src: 'img/logo.svg',
+                },
+                items: [
+                    {to: '/docs/course', label: 'Course', position: 'left'},
+                    {to: '/docs/blog', label: 'Blog', position: 'left'},
+                    {to: '/docs/talk', label: 'Talk', position: 'left'},
+                    {to: '/docs/group', label: 'Group', position: 'left'},
+                    {to: '/docs/paper/intro', label: 'Paper', position: 'left'},
+                    {to: '/docs/tutorial/MLOps_101', label: 'Tutorial', position: 'left'},
+                    // {
+                    //   type: 'doc',
+                    //   docId: 'paper/intro',
+                    //   position: 'left',
+                    //   label: 'Paper',
+                    // },
+                    // {
+                    //   type: 'doc',
+                    //   docId: 'tutorial/MLOps_101',
+                    //   position: 'left',
+                    //   label: 'Tutorial',
+                    // },
+                    // {to: '/blog', label: 'Blog', position: 'left'},
+                    // {to: '/docs/paper', label: 'Paper', position: 'left'},
+                    {
+                        href: 'https://breezeml.ai',
+                        label: 'BreezeML',
+                        position: 'right',
+                    },
+                    {
+                        href: 'https://github.com/HuaizhengZhang/Awesome-System-for-Machine-Learning',
+                        label: 'GitHub',
+                        position: 'right',
+                    },
+
+                ],
+            },
+            footer: {
+                style: 'dark',
+                links: [
+                    {
+                        title: 'Docs',
+                        items: [
+                            {
+                                label: 'Quick Start',
+                                to: '/docs/quick_start',
+                            },
+                        ],
+                    },
+                    {
+                        title: 'Community',
+                        items: [
+                            {
+                                label: 'BreezeML',
+                                href: 'https://breezeml.ai',
+                            },
+                            {
+                                label: 'Twitter',
+                                href: 'https://twitter.com/docusaurus',
+                            },
+
+                        ],
+                    },
+                    {
+                        title: 'More',
+                        items: [
+                            // {
+                            //   label: 'Blog',
+                            //   to: '/blog',
+                            // },
+                            {
+                                label: 'GitHub',
+                                href: 'https://github.com/HuaizhengZhang/Awesome-System-for-Machine-Learning',
+                            },
+
+                        ],
+                    },
+                    {
+                        title: 'Visitor From:',
+                        items: [
+                                                        {
+                                html: `
+<a href="https://info.flagcounter.com/5njl"><img src="https://s11.flagcounter.com/count2/5njl/bg_FFFFFF/txt_000000/border_CCCCCC/columns_2/maxflags_10/viewers_0/labels_1/pageviews_1/flags_0/percent_0/" alt="Flag Counter" border="0"></a>`,
+
+                            },
+                        ],
+                    }
+                ],
+                copyright: `Copyright © ${new Date().getFullYear()} MLSysOps, Inc. Built with Docusaurus.`,
+
+
+            },
+            prism: {
+                theme: lightCodeTheme,
+                darkTheme: darkCodeTheme,
+            },
+            //
+            //   algolia: {
+            //   // The application ID provided by Algolia
+            //   appId: 'YOUR_APP_ID',
+            //
+            //   // Public API key: it is safe to commit it
+            //   apiKey: 'YOUR_SEARCH_API_KEY',
+            //
+            //   indexName: 'YOUR_INDEX_NAME',
+            //
+            //   // Optional: see doc section below
+            //   contextualSearch: true,
+            //
+            //   // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+            //   externalUrlRegex: 'external\\.com|domain\\.com',
+            //
+            //   // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+            //   replaceSearchResultPathname: {
+            //     from: '/docs/', // or as RegExp: /\/docs\//
+            //     to: '/',
+            //   },
+            //
+            //   // Optional: Algolia search parameters
+            //   searchParameters: {},
+            //
+            //   // Optional: path for search page that enabled by default (`false` to disable it)
+            //   searchPagePath: 'search',
+            //
+            //   //... other Algolia params
+            // },
+        }),
 };
 
 module.exports = config;
